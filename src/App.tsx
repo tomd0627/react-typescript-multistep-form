6b6a8fc900dbb40e76f1291d843e8fc513ea558e
Tom DeLuca <tomd0627@optonline.net>
2022-10-14 21:44:25 -0400

    - initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./assets/style.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+function App() {
+  return (
+    <div className="App h-100">
+      <div className="container h-100">
+        <div className='row h-100'>
+          <div className='col-12'>
+            <div className="h-100 d-flex flex-column align-items-center justify-content-center">
+              <h2>Heading</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
